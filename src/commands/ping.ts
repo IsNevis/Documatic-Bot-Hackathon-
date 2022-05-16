1046e82174df88d1ce98ce345aabc2a47288c9a9
IsNevis <94104284+IsNevis@users.noreply.github.com>
2022-05-17 02:26:17 +0530

    Ping Command

---
+++ src/commands/ping.ts
@@ -0,0 +1,19 @@
+import { MessageEmbed } from 'discord.js'
+import { ICommand } from 'wokcommands'
+
+export default {
+	category: 'General',
+	description: 'Replies with Pong!',
+
+	slash: true,
+	guildOnly: true,
+	testOnly: true,
+
+	callback: async ({ }) => {
+        const embed = new MessageEmbed()
+            .setTitle('Ping')
+            .setColor('BLURPLE')
+            .setDescription(':ping_pong: Pong!')
+            return embed
+	},
+} as ICommand
